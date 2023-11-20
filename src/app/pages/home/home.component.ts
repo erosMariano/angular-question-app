a5afb82f0c9e268eb678b378a0099506f023d88d
erosMariano <erosmariano1@gmail.com>
2023-11-20 00:04:10 -0300

    feat: header chat

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { SearchComponent } from '../../components/search/search.component';
 import { ChatQuestionsComponent } from '../../components/chat-questions/chat-questions.component';
+import { HeaderChatComponent } from '../../components/header-chat/header-chat.component';
+import { ChatComponent } from '../../components/chat/chat.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, SearchComponent, ChatQuestionsComponent],
+  imports: [
+    CommonModule,
+    SidebarComponent,
+    SearchComponent,
+    ChatQuestionsComponent,
+    HeaderChatComponent,
+    ChatComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
